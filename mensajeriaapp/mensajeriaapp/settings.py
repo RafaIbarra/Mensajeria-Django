04573a1ed28331588c23f0e59e26f3da0bd5414a
RafaIbarra <blasrafael1986@gmail.com>
2022-12-23 21:38:55 -0300

    configuraciones variables

--- mensajeriaapp/mensajeriaapp/settings.py
+++ mensajeriaapp/mensajeriaapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure-qtpt!eji(9n1!#i4vsy2=15!fsynyli+p*hp81e7@4aqjmwm(8'
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
-
+PYTHON_VERSION=os.getenv('3.10.7')
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 
