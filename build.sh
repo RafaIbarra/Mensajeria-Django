762c8d4b13172ebb32aecd9347998fec5096e415
RafaIbarra <blasrafael1986@gmail.com>
2022-12-23 23:48:04 -0300

    cambio archivo build de carpetas

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ set -o errexit
 # poetry install
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+python ./mensajeriaapp/mensajeriaapp/manage.py collectstatic --no-input
 
-python manage.py migrate
+python ./mensajeriaapp/mensajeriaapp/manage.py migrate
 
-python manage.py tailwind build
+python ./mensajeriaapp/mensajeriaapp/manage.py tailwind build
