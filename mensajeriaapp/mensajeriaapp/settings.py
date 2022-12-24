f411cb79438a86a8d695cd7c7975dc3caf820465
RafaIbarra <blasrafael1986@gmail.com>
2022-12-24 17:47:29 -0300

    cambio configuracion BD 6

--- mensajeriaapp/mensajeriaapp/settings.py
+++ mensajeriaapp/mensajeriaapp/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'mensajeriaapp.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-       default='postgresql://postgres:postgres@localhost/postgres'
+       default='ENGINE:django.db.backends.postgresql_psycopg2'
     )
 }
 # DATABASES = {
