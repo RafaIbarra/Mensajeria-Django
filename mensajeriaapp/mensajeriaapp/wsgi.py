22b36434e955b342f0537e6aa58062db34ebb28b
RafaIbarra <blasrafael1986@gmail.com>
2022-12-24 16:43:19 -0300

    cmabio congifguraciones

--- mensajeriaapp/mensajeriaapp/wsgi.py
+++ mensajeriaapp/mensajeriaapp/wsgi.py
@@ -7,10 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
+import importlib
+module_name = 'package'
+special_module = importlib.import_module(module_name, package=None)
+
+
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', './mensajeriaapp/mensajeriaapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mensajeriaapp.settings')
 
 application = get_wsgi_application()
