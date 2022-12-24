113ed82af93ee1f02342bd443dd4d549d57b4e7e
RafaIbarra <blasrafael1986@gmail.com>
2022-12-24 17:12:28 -0300

    cambio configuracion conexion BD

--- mensajeriaapp/mensajeriaapp/settings.py
+++ mensajeriaapp/mensajeriaapp/settings.py
@@ -84,16 +84,22 @@ WSGI_APPLICATION = 'mensajeriaapp.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
+    'default': dj_database_url.config()
+}
+
+# DATABASES = {
+#     'default': dj_database_url.config(
 
 
-        default='django.db.backends.postgresql_psycopg2',
-        conn_max_age=600
+#         default='django.db.backends.postgresql_psycopg2',
+#         conn_max_age=600
 
 
         
-    )
-}
+#     )
+# }
+
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
